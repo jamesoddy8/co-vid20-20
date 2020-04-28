016e3ffa06f86b02df1a131a132e8800d2ae7c10
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-04-28 16:22:20 +0100

    small refactoring

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_125146) do
+ActiveRecord::Schema.define(version: 2020_04_28_142521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,9 +53,10 @@ ActiveRecord::Schema.define(version: 2020_04_28_125146) do
   create_table "videos", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.references "user"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
