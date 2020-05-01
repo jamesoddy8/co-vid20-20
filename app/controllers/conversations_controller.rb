5f42e36f6072d869d8642867f5c0260272576735
plkujaw <jawor.jakub@gmail.com>
2020-05-01 16:02:04 +0100

    working simple messaging feature

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,14 +1,14 @@
 class ConversationsController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user!
 
 
   def index
     @users = User.all
-    @conversations = Conversations.all
+    @conversations = Conversation.all
   end
 
   def create
-    if Conversation.between(params[:sender_id].params[:recipient_id]).present?
+    if Conversation.between(params[:sender_id],params[:recipient_id]).present?
       @conversation = Conversation.between(params[:sender_id].params[:recipient_id]).first
     else
       @conversation = Conversation.create!(conversation_params)
