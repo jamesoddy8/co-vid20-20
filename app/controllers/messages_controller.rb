a7dfd09f9a6e3ef4cd30afd38cb8de6f7ea18b88
plkujaw <jawor.jakub@gmail.com>
2020-05-05 16:56:36 +0100

    created array with unread messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,7 @@ end
   def index
     @over_ten = false
     @messages = @conversation.messages.order(id: :asc)
+    puts unread_in_conversation
     if @messages.length > 10
       @over_ten = true
       @messages = @messages[-10..-1]
@@ -34,6 +35,20 @@ end
       redirect_to conversation_messages_path(@conversation)
     end
   end
+
+  def unread_in_conversation
+    @unread = []
+    @conversation.messages.each do |message|
+      if message.read == false
+        @unread << message
+      end
+    end
+    # Message.all(:conditions => ["read = ?", false])
+    puts '------------'
+    p @unread
+    # Client.first(:conditions => ["orders_count = ?", params[:orders]])
+  end
+
 
   private
   def message_params
