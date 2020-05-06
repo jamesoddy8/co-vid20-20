1b8aefab559cd8b3fb6e81eae86777dca358502b
john-baxter <john.g.baxter@googlemail.com>
2020-05-06 10:31:55 +0100

    WIP scope issues with .unread_in_conversation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,5 +1,7 @@
 class Conversation < ApplicationRecord
 
+  attr_reader :unread, :output, :current_user
+
   belongs_to :sender, :foreign_key => :sender_id, class_name: "User"
   belongs_to :recipient, :foreign_key => :recipient_id, class_name: "User"
 
@@ -10,18 +12,19 @@ class Conversation < ApplicationRecord
     where("(conversations.sender_id = #{sender_id} AND conversations.recipient_id = #{recipient_id}) OR (conversations.sender_id = #{recipient_id} AND conversations.recipient_id = #{sender_id})")
   end
 
-  # def unread_in_conversation
-  #   @unread = []
-  #     @conversation.messages.each do |message|
-  #       if message.read == false
-  #         # if message.user_id != current_user.id
-  #           @unread << message
-  #         # end
-  #       end
-  #     end
-  #     @unread.length == 0 ? @output = false : @output = true
-  #     return @output
-  #   end
+  def unread_in_conversation(conversation)
+    @unread = []
+    # @current_user = current_user
+      conversation.messages.each do |message|
+        if message.read == false
+          if message.user_id != conversation.recipient_id
+            @unread << message
+          end
+        end
+      end
+      @unread.length == 0 ? @output = false : @output = true
+      return @output
+    end
 
 end
 
